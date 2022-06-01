88a740faec8e3635c59736437d263262962b6ca5
nivpgir <Nivpgir@gmail.com>
2022-06-01 13:12:42 +0300

    WIP - make the autocompletion scrollable
    mostly works, but the it isn't possible to scroll beyond the selected
    item.
    
    and also the calculation of the height of the widget is a little bit
    off.

--- src/autocomplete_popup.rs
+++ src/autocomplete_popup.rs
@@ -1,5 +1,5 @@
 
-use eframe::egui::{self, Modifiers, Widget, WidgetText};
+use eframe::{egui::{self, Modifiers, Widget, WidgetText}, emath::NumExt};
 
 #[derive(Debug)]
 pub(crate) struct AutocompletePopup<C>
@@ -10,7 +10,7 @@ where
     parent: egui::Response,
 }
 
-type Selection = Option<AutocompleteOutput<usize>>;
+type Selection = AutocompleteOutput<usize>;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum AutocompleteOutput<C>{
@@ -18,7 +18,22 @@ pub enum AutocompleteOutput<C>{
     Marked(C),
 }
 
+impl <T: Default> Default for AutocompleteOutput<T>{
+    fn default() -> Self {
+        Marked(T::default())
+    }
+}
+
+impl<T: Copy> Copy for AutocompleteOutput<T> {}
+
 use AutocompleteOutput::*;
+
+// #[derive(Clone, Default)]
+// struct AutocompleteState{
+//     selection: Selection,
+//     scroll_offset: f32
+// }
+
 impl<C> AutocompletePopup<C>
 where
     C: Clone + Into<WidgetText> + std::fmt::Debug,
@@ -36,91 +51,81 @@ where
 	}
     }
 
-    fn create_autocomplete_labels(&self, ui: &mut egui::Ui, marked_value: &Selection)
-				  -> Option<Vec<egui::Response>>{
-
-	egui::popup_below_widget(ui, self.id(), &self.parent, |ui|{
-	    self.items.iter().enumerate()
-		.map(|(i, item)|{
-		    ui.selectable_label(*marked_value == Some(Marked(i)) ,item.clone())
-		})}.collect())
-    }
-
-    fn update_mark_by_keyboard(&mut self, mark: Selection, ui: &mut egui::Ui) -> Selection{
+    fn update_selection_by_keyboard(&self, selection: Selection, ui: &mut egui::Ui) -> Selection{
 	use egui::Key;
-	let ret = if ui.input_mut().consume_key(Modifiers::NONE, Key::ArrowUp){
-	    mark.map(AutocompleteOutput::dec).or(Some(AutocompleteOutput::Marked(0)))
+	if ui.input_mut().consume_key(Modifiers::NONE, Key::ArrowUp){
+	    selection.dec()
 	} else if ui.input_mut().consume_key(Modifiers::NONE, Key::ArrowDown){
-	    mark.map(AutocompleteOutput::inc).or(Some(AutocompleteOutput::Marked(0)))
+	    selection.inc()
 	} else if ui.input_mut().consume_key(Modifiers::NONE, Key::Enter){
-	    mark.map(AutocompleteOutput::into_choice)
-		.or(Some(AutocompleteOutput::Chosen(0)))
+	    selection.into_choice()
 	} else {
-	    mark
-	}.map(|c: AutocompleteOutput<_>|c.clamp(self.items.len()-1));
-	ret
+	    selection
+	}.map(|c|c.at_most(self.items.len().saturating_sub(1)))
     }
 
     pub fn show(&mut self, ui: &mut egui::Ui) -> Option<AutocompleteOutput<C>>{
 	if self.parent.gained_focus() || self.parent.changed(){
-	    ui.memory().open_popup(self.id())
+	    ui.memory().open_popup(self.id());
+	    if self.items.is_empty(){
+		ui.memory().close_popup();
+	    }
 	}
 
-	let mut marked_value = ui.memory().data.get_temp::<AutocompleteOutput<usize>>(self.id());
+	let popup_response = egui::popup_below_widget(
+	    ui, self.id(), &self.parent, self.make_completion_widget()
+	);
 
-	marked_value = self.update_mark_by_keyboard(marked_value.clone(), ui);
-	if let Some(label_responses) = self.create_autocomplete_labels(ui, &marked_value){
-	    marked_value = update_mark_by_mouse_interaction(marked_value.clone(), label_responses);
-	}
-
-	if let Some(AutocompleteOutput::Chosen(_)) = &marked_value {
+	if let Some(Chosen(_)) = dbg!(popup_response) {
 	    ui.memory().data.remove::<AutocompleteOutput<usize>>(self.id());
 	    ui.memory().close_popup();
 	    self.parent.request_focus();
-	} else if let Some(mark) = marked_value.clone(){
-	    ui.memory().data.insert_temp(self.id(), mark)
+	} else if let Some(selection) = popup_response{
+	    ui.memory().data.insert_temp(self.id(), selection)
 	}
-	marked_value.map(|c|c.map(&self.items))
+	popup_response.map(|c|c.map(|i|self.items[i].clone()))
+    }
+
+    fn make_completion_widget(&self) -> impl FnOnce(&mut egui::Ui) -> Selection + '_{
+	|ui: &mut egui::Ui|{
+	    let prev_selection = *ui.memory().data
+		.get_temp_mut_or_default::<AutocompleteOutput<usize>>(self.id());
+	    let prev_scroll = *ui.memory().data.get_temp_mut_or_default::<f32>(self.id());
+	    let spacing = ui.spacing().item_spacing.y;
+	    let row_height = ui.text_style_height(&egui::TextStyle::Body);
+	    let selection_pos = prev_selection.into_inner() as f32 *
+		(row_height + spacing);
+	    let rect_height = ui.max_rect().height() - ui.max_rect().top();
+	    let new_scroll = prev_scroll
+		.at_least(selection_pos - rect_height + row_height)
+		.at_most(selection_pos);
+	    let selected_index = prev_selection.into_inner();
+	    let scroll = egui::ScrollArea::vertical()
+		.vertical_scroll_offset(new_scroll)
+		.show_rows(ui, row_height, self.items.len(), |ui, rows|{
+		    rows.map(|row_num|(row_num, ui.selectable_label(selected_index == row_num, self.items[row_num].clone())))
+			.filter_map(check_mouse_interactions)
+			.find(|s|s.is_chosen())
+		});
+	    let keyboard_selection = self.update_selection_by_keyboard(prev_selection, ui);
+	    ui.memory().data.insert_temp(self.id(), scroll.state.offset.y);
+	    scroll.inner.unwrap_or(keyboard_selection)
+	}
     }
 }
 
-
-fn check_mouse_interactions((i, response): (usize, egui::Response)) -> Selection{
+fn check_mouse_interactions((i, response): (usize, egui::Response)) -> Option<Selection>{
     if response.clicked(){
-	Some(AutocompleteOutput::Chosen(i))
+	Some(Chosen(i))
     } else if response.ctx.input().pointer.is_moving() && response.hovered() {
 	// TODO: BUG:
 	// for some reason response.hovered() is always false
-	Some(AutocompleteOutput::Marked(i))
+	Some(Marked(i))
     } else {
 	None
     }
 }
 
-
-fn update_mark_by_mouse_interaction(current_mark: Selection, mouse_interactions: Vec<egui::Response>)
-				    -> Selection{
-    use std::ops::ControlFlow;
-    let new_mark = mouse_interactions.into_iter().enumerate()
-	.map(check_mouse_interactions)
-	.try_fold(current_mark, |prev, next|{
-	    if let (Some(p), Some(n)) = (prev.clone(), next.clone()){
-		if let AutocompleteOutput::Chosen(i) = p.clone().update(n){
-		    ControlFlow::Break(Some(AutocompleteOutput::Chosen(i)))
-		} else{
-		    ControlFlow::Continue(Some(p))
-		}
-	    } else {
-		ControlFlow::Continue(prev.or(next))
-	    }
-	});
-    match new_mark{
-	// for some reason there is no way of getting the inner value without matching,
-	// even when they both hold the same type, idk why...
-	ControlFlow::Break(v) => v,
-	ControlFlow::Continue(v) => v,
-    }
-}
 
 impl<C> Widget for AutocompletePopup<C>
 where
@@ -150,13 +155,13 @@ impl AutocompleteOutput<usize> {
 	    Self::Marked(i) => Self::Marked(i.saturating_add(1)),
 	}
     }
-    pub(crate) fn clamp(self, max: usize) -> Self {
-	match self {
-	    Self::Chosen(i) => Self::Chosen(i.clamp(0, max)),
-	    Self::Marked(i) => Self::Marked(i.clamp(0, max)),
-	}
-    }
 
+    fn is_chosen(&self) -> bool {
+        matches!(self, Chosen(_))
+    }
+}
+
+impl<T> AutocompleteOutput<T> {
     pub(crate) fn into_choice(self) -> Self {
         match self{
 	    Self::Marked(i) => Self::Chosen(i),
@@ -164,19 +169,17 @@ impl AutocompleteOutput<usize> {
 	}
     }
 
-    pub(crate) fn update(self, other: Self) -> Self{
-	match (self, other){
-	    (Self::Chosen(i), _) |
-	    (Self::Marked(_), Self::Chosen(i)) => Self::Chosen(i),
-	    (Self::Marked(i), Self::Marked(_)) => Self::Marked(i),
+    fn map<C, F: FnOnce(T) -> C>(self, f: F) -> AutocompleteOutput<C>{
+	match self{
+	    Self::Chosen(i) => Chosen(f(i)),
+	    Self::Marked(i) => Marked(f(i)),
 	}
     }
-    fn map<C>(self, items: &[C]) -> AutocompleteOutput<C>
-    where C: Clone + Into<WidgetText> + std::fmt::Debug{
-	match self{
-	    Self::Chosen(i) => AutocompleteOutput::Chosen(items[i].clone()),
-	    Self::Marked(i) => AutocompleteOutput::Marked(items[i].clone()),
-	}
 
+    pub fn into_inner(self) -> T{
+	match self{
+	    Self::Chosen(i) |
+	    Self::Marked(i) => i,
+	}
     }
 }
