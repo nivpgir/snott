5ed510d7ca1dc411e9628b79878732ffe0364aaa
Niv Pagir <nivpgir@gmail.com>
2022-07-17 20:02:58 +0300

    font size configuration options

--- src/quick_snote_main.rs
+++ src/quick_snote_main.rs
@@ -26,7 +26,13 @@ fn main() {
 		.map(tilde).unwrap().as_ref()
 		.into();
 	    let time_format = settings.get_string("timestamp_format").unwrap();
-            cc.egui_ctx.set_visuals(eframe::egui::Visuals::dark());
+	    let body_font_size = settings.get_float("body_font_size").unwrap() as f32;
+	    let headline_font_size = settings.get_float("headline_font_size").unwrap() as f32;
+	    let mut s = eframe::egui::Style::default();
+	    s.text_styles.insert(egui::TextStyle::Body, egui::FontId::proportional(body_font_size));
+	    s.text_styles.insert(egui::TextStyle::Heading, egui::FontId::proportional(headline_font_size));
+	    cc.egui_ctx.set_style(s);
+	    cc.egui_ctx.set_visuals(eframe::egui::Visuals::dark());
             Box::new(QuickSnote::new(sync_dir)
 		     .with_time_format(time_format))
         }),
@@ -45,6 +51,8 @@ fn build_config() -> config::Config{
         .add_source(config::Environment::with_prefix("SNOTT"))
         .set_default("sync_dir", home_dir.display().to_string()).unwrap()
         .set_default("timestamp_format", "%Y-%m-%d_%H-%M-%S").unwrap()
+        .set_default("body_font_size", "20").unwrap()
+        .set_default("headline_font_size", "40").unwrap()
         .build()
         .unwrap()
 }
